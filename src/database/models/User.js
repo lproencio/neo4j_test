3f6129070963639da15e2a7953a2ef27784065e1
lproencio <leandroproencio@gmail.com>
2021-12-31 13:02:39 -0300

    add info

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,13 +1,17 @@
 const instance = require("../index.js");
 
 const User = instance.model("User", {
+  id: {
+    type: "uuid",
+    primary: true,
+  },
   name: {
     type: "string",
   },
   cpf: { type: "string", unique: "true", required: true, length: 11 },
   email: { type: "email", unique: "true", required: true },
   phone: { type: "string" },
-  role: { type: "string", required: true },
+  is_active: { type: "boolean", default: true },
   created_at: { type: "datetime" },
   updated_at: { type: "datetime" },
 });
